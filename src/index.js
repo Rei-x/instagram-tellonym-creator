d90944b3c9e869886aa8ab65c6294f8fff34fbe3
Rei-666 <wszyzdgo@gmail.com>
2021-01-02 15:34:47 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./assets/css/style.scss"
+console.log('Hello World from your main file!');
+document.querySelector(".header").innerHTML = "kurwaaa"
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = (_, {mode}) => ({
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: './'
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          mode === "production" ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(svg|jpg|gif|png)$/,
+        use: 'file-loader'
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
+    new CleanWebpackPlugin()
+  ]
+});
+
+module.exports = config;
